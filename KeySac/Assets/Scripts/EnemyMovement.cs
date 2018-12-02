30ef827d3747c46ea48a82389a816579b7a8a1fd
EMo <bleuflmo@hotmail.co.uk>
2018-12-02 15:39:02 +0000

    Enemy Movement WIP
    WIP fixing enemy movement to collide with walls

--- KeySac/Assets/Scripts/EnemyMovement.cs
+++ KeySac/Assets/Scripts/EnemyMovement.cs
@@ -5,31 +5,52 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour {
 	public int health;
+    public float speed;
     Transform player;
     NavMeshAgent nav;
 
 	// Use this for initialization
 	void Start () {
-        //player = GameObject.FindGameObjectWithTag("Player").transform;
+
         nav = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //nav.SetDestination(player.position);
+        speed = 4f;
+
+        Move();
+
 		if (health <= 0) {
 			Destroy (gameObject);
 		}
     }
+
+    //Take damage function
 	public void damage(int dam){
 		health -= dam;
 	}
 
+    //Move towards player's last known position if player intercepts field of vision trigger
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             nav.GetComponent<NavMeshAgent>().SetDestination(other.transform.position);
+        }
+    }
+
+    //Movement - patrols in straight line (unless player is seen)
+    void Move() {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    //TODO: Turn around on collision with a wall
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Walls")
+        {
+            transform.rotation = Quaternion.AngleAxis(180, transform.up) * transform.rotation;
         }
     }
 }
