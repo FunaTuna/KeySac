56941bb8c5774ffae968a1849357dde7f42ad967
EMo <bleuflmo@hotmail.co.uk>
2018-12-02 00:32:09 +0000

    Added bullet script
    Bullet scripts

---
+++ KeySac/Assets/BulletScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Destroy on collision with wall or enemy
+    void DestroyOnCollision (Collision other) {
+        if (other.gameObject.CompareTag("Walls") || other.gameObject.CompareTag("Enemy")) {
+            Destroy(gameObject);
+        }
+    }
+}
