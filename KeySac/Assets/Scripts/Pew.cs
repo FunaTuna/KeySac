ae7b5ad848b194221c10cf379f87263a2e45ef17
EMo <bleuflmo@hotmail.co.uk>
2018-12-02 23:20:59 +0000

    AudioSource and pew.ogg moved
    moved pew.ogg into Assets and created AudioSource object

---
+++ KeySac/Assets/Scripts/Pew.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pew : MonoBehaviour {
+
+    public AudioClip pewClip;
+    public AudioSource pewSource;
+
+	// Use this for initialization
+	void Start () {
+        pewSource.clip = pewClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
